6b0a76986035a3c4c7a4071061cf4fa4cafb6efe
Bramantyoadi12 <56532145+Bramantyoadi12@users.noreply.github.com>
2020-10-19 17:12:56 +0700

    First Commit

--- ServerConfigMtk-P2-20180140052/Program.cs
+++ ServerConfigMtk-P2-20180140052/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceMtk_P1_20180140052;
 
 namespace ServerConfigMtk_P2_20180140052
 {
@@ -10,6 +12,21 @@ namespace ServerConfigMtk_P2_20180140052
     {
         static void Main(string[] args)
         {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(Matematika));
+                hostObj.Open();
+                Console.WriteLine("Server is Ready!!!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch(Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
         }
     }
 }
